c31fd6373623e15205b1c66c18afb92430f196fb
zincfan <57042418+zincfan@users.noreply.github.com>
2020-04-05 20:11:09 +0530

    5-apr changes

--- flask_hello.py
+++ flask_hello.py
@@ -1,6 +1,8 @@
 # Import the Flask package
 import flask
 from flask import Flask,render_template,redirect,session
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 import os.path
 from os import path
 import hashlib
@@ -8,21 +10,39 @@ import hashlib
 
 # Initialize Flask
 app = Flask(__name__)
+app_limiter=Limiter(
+       app,
+       key_func=get_remote_address,
+       default_limits=["200 per day","60 per hour"]
+)
 
 # Define the index route
 @app.route("/")
 def index():
    return render_template("index.html")
 
+@app.route('/favicon.ico')
+def logo():
+    return redirect('static/icons/favicon.png', code=302)
+
 @app.route("/createpro")
 def createpro():
        return render_template("create-profile.html")
 
+@app.route("/test")
+def test():
+       return render_template("test.html")
+
 @app.route("/seehim")
 def seehim():
-       return "wait until you see him. Oh no no no no oo oo oo ha ha ha - _ - !!"
+    return f'''wait until you see him. Oh no no no no oo oo oo ha ha ha - _ - !! <iframe width="560" height="315" src="https://www.youtube.com/embed/7wivOEXlL9s?end=19" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'''
+
+@app.route("/testmetro")
+def testmetro():
+       return render_template("testmetro.html")
 
 @app.route("/login",methods=["GET","POST"])
+@app_limiter.limit("15 per day")
 def login():
        if flask.request.method == "POST":
             username = flask.request.values.get('userid')
@@ -39,18 +59,18 @@ def login():
                                      session.permanent = True
                                  return redirect("/",code=302)
                           else:
-                                 return render_template("login.html",error="Password doesnot match.")
+                                 return render_template("login.html",error="Password doesnot match.",code=401)
                    except OSError:
-                            return render_template("login.html", error="Temporary Error.Try later.")
+                            return render_template("login.html", error="Temporary Error.Try later.",code=400)
                    except AssertionError:
-                            return render_template("login.html", error="Username doesnot exists.")
-       return render_template("login.html")
+                            return render_template("login.html", error="Username doesnot exists.",code=401)
+       return render_template("login.html",code=200)
 
 @app.route('/logout')
 def logout():
    # remove the username from the session if it is there
    session.pop('username', None)
-   return redirect('login',code=302)
+   return redirect('/',code=302)
 
 @app.route("/data",methods=["GET",'POST'])
 def data():
@@ -70,15 +90,17 @@ def data():
                    except OSError:
                             return "couldnot create.Temporary error"
                    except AssertionError:
-                            return f"{username} already exists"
+                            return render_template("create-profile.html",error="Username exists",code=401)
        else:
               return "no credentials recieved"
        return "NULL"
 
 
+
+
 # Run Flask if the __name__ variable is equal to __main__
 if __name__ == "__main__":
-       app.secret_key = 'super secret_key'
+       app.secret_key = 'superfy6secret_g4ni7key'
        app.config['SESSION_TYPE'] = 'filesystem'
        app.config["PERMANENT_SESSION_LIFETIME"]=31
        app.run(debug=True)
---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+var app4 = new Vue({
+  el: '#top-information',
+  data: {
+    todos: [
+      { title:"update" ,text: 'Learn JavaScript' },
+      { title:"update" ,text: 'Learn Vue' },
+      { title:"update" ,text: 'Build something awesome' }
+    ]
+  }
+})
